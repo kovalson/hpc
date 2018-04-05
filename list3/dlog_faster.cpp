121e4b6c155cca89146e80f0a297c09d500d637d
Krzysztof Tatarynowicz <ktatarynowicz@hotmail.com>
2018-04-05 12:38:35 +0200

    Poprawka

--- list3/dlog_faster.cpp
+++ list3/dlog_faster.cpp
@@ -6,19 +6,32 @@
  * Lista 3
  */
 
+/*
+ 	Drugi przykład z laboratorium:
+		p = 1263154214185307
+		g = 4
+		h = 473567883536982
+
+	Trzeci przykład z laboratorium:
+		h = 262635754439740
+*/
+
 #include <omp.h>
 #include <NTL/ZZ.h>
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <set>
 #include <time.h>
 
 #define NO_ERRORS		0
-#define THREADS_NUM		2
-#define BITS_NUM		11
+#define THREADS_NUM		4
 
 using namespace std;
 NTL_CLIENT
+
+int BITS_NUM_MIN	= 41;
+int BITS_NUM_MAX	= 41;
 
 // Klasa punktu rozróżnialnego
 class DPoint
@@ -27,19 +40,7 @@ public:
 	ZZ x;
 	ZZ a;
 	ZZ b;
-
-	bool equals( DPoint point );
 };
-
-bool DPoint::equals( DPoint point )
-{
-	// Sprawdź, czy wszystkie atrybuty mają taką samą wartość
-	return (
-		this->x == point.x &&
-		this->a == point.a &&
-		this->b == point.b
-	);
-}
 
 // Funkcja z zadania 1.
 void cycle_vals( ZZ &x, ZZ &a, ZZ &b, ZZ v1, ZZ v2, ZZ p, ZZ q )
@@ -70,7 +71,8 @@ bool is_distinguished( ZZ num )
 	// Sprawdź, czy liczba bitów potrzebnych
 	// do zapisu podanej wartości jest mniejsza niż
 	// pewna stała BITS_NUM
-	return NumBits( num ) < BITS_NUM;
+	long num_bits = NumBits( num );
+	return num_bits == BITS_NUM_MAX;
 }
 
 bool add_distinguished( vector<DPoint> *distinguished, DPoint point, ZZ g, ZZ h, ZZ n )
@@ -78,38 +80,31 @@ bool add_distinguished( vector<DPoint> *distinguished, DPoint point, ZZ g, ZZ h,
 	// Sprawdź, czy jest już taki punkt
 	for( int i = 0; i < distinguished->size(); i++ )
 	{
-		// Jeśli jest
-		if( distinguished->at( i ).x == point.x &&
-			(distinguished->at( i ).a != point.a ||
-			 distinguished->at( i ).b != point.b )
-		)
+		if( distinguished->at( i ).x == point.x )
 		{
 			// Oblicz logarytm
 			// d - b
 			ZZ d_min_b = (point.b - distinguished->at( i ).b) % n;
 			if( d_min_b == 0 )
 			{
-				cout << "Nie ma rozwiązania..." << endl;
+				return false;
+				// cout << "Nie ma rozwiązania..." << endl;
 			}
 			else
 			{
 				cout << "x = " << InvMod( d_min_b, n ) * (distinguished->at( i ).a - point.a) % n << endl;
+				return true;
 			}
-
-
-			// Zwróć informację, że była kolizja
-			return true;
 		}
 	}
 
-	// Jak nie ma kolizji - dodaj punkt
 	distinguished->push_back( point );
 
 	// Zwróć informację, że nie było kolizji
 	return false;
 }
 
-void find_x( ZZ p, ZZ g, ZZ h )
+bool find_x( ZZ p, ZZ g, ZZ h )
 {
 	ZZ q;
 	ZZ x, a, b;
@@ -133,13 +128,15 @@ void find_x( ZZ p, ZZ g, ZZ h )
 	// Flaga informująca o skończeniu obliczeń (znalezieniu dloga)
 	bool flag = false;
 
+	int max = 100000;
+
 	#pragma omp parallel for private( x, a, b, X, A, B, tid ) firstprivate( counter ) shared( distinguished, flag ) num_threads( THREADS_NUM )
-	for( int i = 0; i < 100000; i++ )
+	for( int i = 0; i < max; i++)
 	{
 
 		// Jeśli znaleziono logarytm - skończ iteracje
 		if( flag ) continue;
-		
+
 		// Sekcja krytyczna losowania liczb, bo inaczej
 		// świat płonie i pożoga narusza pamięć
 		#pragma omp critical
@@ -156,25 +153,24 @@ void find_x( ZZ p, ZZ g, ZZ h )
 		A = a;
 		B = b;
 
-		ZZ n;
-		for( n = 0; n < q; n++ )
+		for( int n = 0; n < 1000; n++ )
 		{
 			cycle_vals( x, a, b, g, h, p, q );
 
 			cycle_vals( X, A, B, g, h, p, q );
 			cycle_vals( X, A, B, g, h, p, q );
 
+			// Jeśli znaleziono logarytm - przerwij tę pętlę
+			if( flag ) break;
 
 			// Jeżeli wyliczony punkt jest rozróżnialny
 			if( is_distinguished( x ) )
 			{
+				counter = 0;
 				DPoint dpoint;
 				dpoint.x = x;
 				dpoint.a = a;
 				dpoint.b = b;
-
-				// Jeśli znaleziono logarytm - przerwij tę pętlę
-				if( flag ) break;
 
 				#pragma omp critical
 				{
@@ -184,25 +180,61 @@ void find_x( ZZ p, ZZ g, ZZ h )
 					}
 				}
 			}
+
+
+			if( x == X )
+			{
+				ZZ d_min_b = (B - b) % q;
+				if( d_min_b == 0 )
+				{
+					// cout << "Nie ma rozwiązania..." << endl;
+				}
+				else
+				{
+					// Znaleziono rozwiązanie
+					flag = true;
+
+					cout << "x = " << InvMod( d_min_b, q ) * (a - A) % q << endl;
+				}
+			}
+
 		}
 
 	}
 
+	if( flag ) return true;
+	return find_x( p, g, h );
+
 }
 
-int main()
+int main( int argc, char *argv[] )
 {
 	
 	ZZ p, g, h;
 
-	cout << "Podaj p (strong-prime): ";
-	cin >> p;
+	if( argc > 1 )
+	{
+		p = 1263154214185307;
+		g = 4;
+		int ver = atoi( argv[ 1 ] );
+		if( ver == 1 ) h = 473567883536982;
+		else h = 262635754439740;
+	}
+	else
+	{
+		cout << "Podaj p (strong-prime): ";
+		cin >> p;
 
-	cout << "Podaj g (generator): ";
-	cin >> g;
+		cout << "Podaj g (generator): ";
+		cin >> g;
 
-	cout << "Podaj h (element): ";
-	cin >> h;
+		cout << "Podaj h (element): ";
+		cin >> h;
+	}
+
+	// Ustaw liczbę bitów, które muszą być równe 0
+	// N_BITS = sqrt( NumBits( p ) );
+	// cout << N_BITS << endl;
 
 	// Znajdź x takie, że g^x = h
 	find_x( p, g, h );
