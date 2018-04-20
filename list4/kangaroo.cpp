a431f4c3e49e51edf3774b417e5a88c02578729a
Krzysztof Tatarynowicz <ktatarynowicz@hotmail.com>
2018-04-20 16:35:41 +0200

    Update'y

--- list4/kangaroo.cpp
+++ list4/kangaroo.cpp
@@ -15,19 +15,21 @@
 NTL_CLIENT;
 
 #define	NUM_THREADS		4
-#define BITS_NUM_MAX	32
+#define BITS_NUM_MAX	45
 #define	k				20
 #define	KANGAROO_TAME	0
 #define	KANGAROO_WILD	1
-#define LAST_BITS_NUM	4
+#define LAST_BITS_NUM	1
 using namespace std;
 
 class DPoint
 {
 public:
-	int type;
+	int tag;
 	int idx;
 	ZZ distance;
+	ZZ start;
+	ZZ start_offset;
 };
 
 // long k = 20;
@@ -45,11 +47,11 @@ bool is_distinguished( ZZ num )
 bool is_distinguished2( ZZ num )
 {
 	// Sprawdź czy ostatnie LAST_BITS_NUM
-	// jest 0
+	// jest 1
 	long b = 0;
 	while( b < LAST_BITS_NUM )
 	{
-		if( bit( num, b ) != 0 )
+		if( bit( num, b ) != 1 )
 			return false;
 		b++;
 	}
@@ -68,90 +70,119 @@ ZZ f( ZZ x )
 
 void find_x( ZZ p, ZZ g, ZZ h, ZZ a, ZZ b )
 {
-	long N = conv<long>( (f( (ZZ) 0 ) + f( b )) / 2 * 2 );
+	// long N = conv<long>( (f( (ZZ) 0 ) + f( b )) / 2 * 2 );
 	bool found_flag = false;
-	int idx, type;
-	ZZ v, pos, d;
+	int idx, tag;
+	ZZ v, pos, d, start, exponent;
 	v = ((beta( a, b ) / (NUM_THREADS / 2)));
-	unordered_map<long, DPoint> dpoints;
+	unordered_map<long long, DPoint> dpoints;
+	DPoint dpoint;
 
-	#pragma omp parallel num_threads( NUM_THREADS ) private( pos, d, idx, type ) shared( p, v, dpoints, found_flag )
+	#pragma omp parallel private( pos, d, idx, tag, start, dpoint ) num_threads( NUM_THREADS )
 	{
-		int idx = omp_get_thread_num();
-		int type;
-		ZZ pos, d;
+		// Pobierz id wątku
+		idx = omp_get_thread_num();
 
-		// Wyznaczanie typu kangura
-		if( idx % 2 == 0 )
-			type = KANGAROO_TAME;
-		else
-			type = KANGAROO_WILD;
+		// Wyznacz typ kangura
+		tag = (idx % 2 == 0) ? KANGAROO_TAME : KANGAROO_WILD;
 
 		// Dopuszczaj tylko indeksy 0, 1 dla obu typów kangurów
 		idx /= 2;
 
-		// Początkowy dystans i położenie kangura
-		d = idx * v;
-		if( type == KANGAROO_TAME )
-			pos = PowerMod( g, ((a + b) / 2) + (idx * v), p );
+		// Początkowe położenie kangurów
+		if( tag == KANGAROO_TAME )
+		{
+			start = ((a + b) / 2) + (idx * v);
+
+			#pragma omp critical
+			pos = PowerMod( g, start, p );
+		}
 		else
-			pos = MulMod( h, PowerMod( g, idx * v, p ), p );
+		{
+			start = (idx * v);
+
+			#pragma omp critical
+			pos = MulMod( h, PowerMod( g, start, p ), p );
+		}
+
+		// Początkowa całkowita długość podróży kangura
+		d = idx * v;
 
 		// W nieskończoność
 		while( true )
 		{
+			// Zwiększ dystans i wykonaj skok
+			#pragma omp critical
+			{
+				d += f( pos );
+				pos = MulMod( pos, PowerMod( g, f( pos ), p ), p );
+			}
+
 			// Jeżeli znaleziono już rozwiązanie - przerwij
 			if( found_flag )
 				break;
 
-			// Wykonaj kolejny skok i zaktualizuj dystans
-			pos = MulMod( pos, PowerMod( g, f( pos ), p ), p );
-			d += f( pos );
-
 			// Sekcja krytyczna punktów rozróżnialnych
 			#pragma omp critical
 			{
-				if( is_distinguished2( pos ) )
+				if( is_distinguished( pos ) )
 				{
-					DPoint dpoint;
-					dpoint.type = type;
+					// Stwórz obiekt punktu rozróżnialnego
+					dpoint.tag = tag;
 					dpoint.distance = d;
 					dpoint.idx = idx;
+					dpoint.start = start;
+					dpoint.start_offset = idx * v;
+
+					// Konwersja pozycji na long
 					long at = conv<long>( pos );
 
 					// Jeśli taki punkt jeszcze nie istnieje - dopisz do zbioru
 					if( dpoints.find( at ) == dpoints.end() )
 					{
-						dpoints[ conv<long>( pos ) ] = dpoint;
+						dpoints[ at ] = dpoint;
 					}
 
 					// A jeśli istnieje, to występuje kolizja
 					else
 					{
 						// Interesuje nas kolizja dwóch różnych typów kangurów (wild vs. tame)
-						if( type != dpoints[ at ].type && !found_flag )
+						if( dpoints[ at ].tag != tag && !found_flag && dpoints[ at ].idx == idx )
 						{
-							ZZ tame_d, tame_idx;
-							ZZ wild_d, wild_idx;
+							ZZ tame_d, tame_idx, tame_start, tame_start_offset;
+							ZZ wild_d, wild_idx, wild_start, wild_start_offset;
 
-							if( type == KANGAROO_WILD )
+							if( tag == KANGAROO_WILD )
 							{
 								wild_d = d;
 								wild_idx = idx;
+								wild_start = start;
+								wild_start_offset = idx * v;
 								tame_d = dpoints[ at ].distance;
 								tame_idx = dpoints[ at ].idx;
+								tame_start = dpoints[ at ].start;
+								tame_start_offset = dpoints[ at ].start_offset;
 							}
 							else
 							{
 								tame_d = d;
 								tame_idx = idx;
+								tame_start = start;
+								tame_start_offset = idx * v;
 								wild_d = dpoints[ at ].distance;
 								wild_idx = dpoints[ at ].idx;
+								wild_start = dpoints[ at ].start;
+								wild_start_offset = dpoints[ at ].start_offset;
 							}
 
-							ZZ x = ((a + b) / 2) + tame_d - wild_d;
+							ZZ modulus = (p - 1) / 2;
+							// ZZ x = (((a + b) / 2) % modulus) + (tame_d % modulus) - (wild_d % modulus);
+							// ZZ x = ((a + b) / 2) - tame_start_offset + tame_d - wild_d + wild_start_offset;
+							ZZ x = ((((a + b) / 2))) + (tame_d) - (wild_d);
 
-							cout << "x = " << x << endl;
+							cout << x % modulus << endl;
+							// cout << "at: " << at << endl;
+							// cout << "x = " << x << endl;
 							found_flag = true;
 						}
 					}
@@ -167,6 +198,7 @@ int main()
 	cout << "Podaj p, g, h, a, b: ";
 	cin >> p >> g >> h >> a >> b;
 	
+	// test( p, g, h, a );
 	find_x( p, g, h, a, b );
 
 	return 0;
